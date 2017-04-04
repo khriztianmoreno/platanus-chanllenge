1e3adbcf87c3a287f867202213206f3a94a7aa1f
khriztianmoreno <khriztianmoreno@gmail.com>
2017-04-04 10:15:21 -0500

    Add table for the show metrics

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>By @khriztianmoreno</h2>
+    <table class="table">
+			<thead>
+				<tr>
+					<th>BID</th>
+					<th>BID_SIZE</th>
+					<th>ASK</th>
+					<th>ASK_SIZE</th>
+					<th>BID vs ASK</th>
+					<th>BID_S vs ASK_S</th>
+					<th>$ BID</th>
+					<th>$ ASK</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
   </div>
 </template>
 
@@ -25,7 +26,7 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'BITCOINS - PLATANUS',
     };
   },
 };
@@ -37,17 +38,4 @@ h1, h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
