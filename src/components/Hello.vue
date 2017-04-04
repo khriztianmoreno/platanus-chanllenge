8f5ed859813d84d178885129b9df40f4284ee3b8
khriztianmoreno <khriztianmoreno@gmail.com>
2017-04-04 10:27:17 -0500

    Definition of the data structure to display in the table

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,12 +10,22 @@
 					<th>ASK</th>
 					<th>ASK_SIZE</th>
 					<th>BID vs ASK</th>
-					<th>BID_S vs ASK_S</th>
+					<th>BID_SIZE vs ASK_SIZE</th>
 					<th>$ BID</th>
 					<th>$ ASK</th>
 				</tr>
 			</thead>
 			<tbody>
+				<tr v-for="item in myBitCoins">
+					<td>{{item.BID}}</td>
+					<td>{{item.BID_SIZE}}</td>
+					<td>{{item.ASK}}</td>
+					<td>{{item.ASK_SIZE}}</td>
+					<td>{{item.differenceBidAsk}}</td>
+					<td>{{item.differenceBidSizeAskSize}}</td>
+					<td>{{item.netoBID}}</td>
+					<td>{{item.netoASK}}</td>
+				</tr>
 			</tbody>
 		</table>
   </div>
@@ -27,6 +37,58 @@ export default {
   data() {
     return {
       msg: 'BITCOINS - PLATANUS',
+      myBitCoins: [
+        {
+          BID: 3,
+          BID_SIZE: 907.7,
+          ASK: 8.304992,
+          ASK_SIZE: 907.78,
+          differenceBidAsk: 12.48,
+          differenceBidSizeAskSize: -57.12,
+          netoBID: -0.0592,
+          netoASK: 907.88,
+        },
+        {
+          BID: 3,
+          BID_SIZE: 907.7,
+          ASK: 8.304992,
+          ASK_SIZE: 907.78,
+          differenceBidAsk: 12.48,
+          differenceBidSizeAskSize: -57.12,
+          netoBID: -0.0592,
+          netoASK: 907.88,
+        },
+        {
+          BID: 3,
+          BID_SIZE: 907.7,
+          ASK: 8.304992,
+          ASK_SIZE: 907.78,
+          differenceBidAsk: 12.48,
+          differenceBidSizeAskSize: -57.12,
+          netoBID: -0.0592,
+          netoASK: 907.88,
+        },
+        {
+          BID: 3,
+          BID_SIZE: 907.7,
+          ASK: 8.304992,
+          ASK_SIZE: 907.78,
+          differenceBidAsk: 12.48,
+          differenceBidSizeAskSize: -57.12,
+          netoBID: -0.0592,
+          netoASK: 907.88,
+        },
+        {
+          BID: 3,
+          BID_SIZE: 907.7,
+          ASK: 8.304992,
+          ASK_SIZE: 907.78,
+          differenceBidAsk: 12.48,
+          differenceBidSizeAskSize: -57.12,
+          netoBID: -0.0592,
+          netoASK: 907.88,
+        },
+      ],
     };
   },
 };
